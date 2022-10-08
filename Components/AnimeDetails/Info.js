64cc6503c9445dc20b272bc8dd85a308ca3d3510
abc <abc@gmail.com>
2022-10-08 20:55:58 +0700

    fix reponsive

--- Components/AnimeDetails/Info.js
+++ Components/AnimeDetails/Info.js
@@ -1,59 +1,51 @@
 const Info = (data) => {
-    const animeStudio = []
-    return ( 
-        <div className="anime-info-table grid mt-1">
-            <div className="xl:col-3 md:col-6">
-                <div className="anime-info">
-                    <span className="font-semibold">Format: </span>
-                    {data.data.data.type}
-                </div>
-            </div>
-            <div className="xl:col-3 md:col-6">
-                <div className="anime-info">
-                    <span className="font-semibold">Source: </span>
-                    {data.data.data.source}
-                    </div>
-                </div>
-            <div className="xl:col-3 md:col-6">
-                <div className="anime-info">
-                    <span className="font-semibold">Episodes: </span>
-                    {data.data.data.episodes ? data.data.data.episodes : '--'}
-                </div>
-            </div>
-            <div className="xl:col-3 md:col-6">
-                <div className="anime-info">
-                    <span className="font-semibold">Runtime: </span>
-                    {data.data.data.duration}
-                </div>
-            </div>
-            <div className="col-6">
-                <div className="anime-info">
-                    <span className="font-semibold">Studio: </span>
-
-                    {data.data.data.studios.map(item => 
-                        item.name
-                    ).join(' ')
-                    }
-                    
-                </div>
-            </div>
-            <div className="col-6">
-                <div className="anime-info">
-                    <span className="font-semibold">Tags: </span>
-                    {data.data.data.genres.map((item) => 
-                        item.name
-                    ).join(' ')
-                    
-                        }
-                </div>
-            </div>
-            <div className="col-12 anime-desc">
-                <div className="anime-info">
-                    {data.data.data.synopsis}
-                </div>
-            </div>
+  const animeStudio = [];
+  return (
+    <div className="anime-info-table grid mt-1 pd-1">
+      <div className="anime-info-list">
+        <div className="xl:col-3 md:col-6 sm:col-12">
+          <div className="anime-info">
+            <span className="font-semibold">Format: </span>
+            {data.data.data.type}
+          </div>
         </div>
-    );
-}
- 
+        <div className="xl:col-3 md:col-6 sm:col-12">
+          <div className="anime-info">
+            <span className="font-semibold">Source: </span>
+            {data.data.data.source}
+          </div>
+        </div>
+        <div className="xl:col-3 md:col-6 sm:col-12">
+          <div className="anime-info">
+            <span className="font-semibold">Episodes: </span>
+            {data.data.data.episodes ? data.data.data.episodes : "--"}
+          </div>
+        </div>
+        <div className="xl:col-3 md:col-6 sm:col-12">
+          <div className="anime-info">
+            <span className="font-semibold">Runtime: </span>
+            {data.data.data.duration}
+          </div>
+        </div>
+        <div className="xl:col-6 md:col-6 sm:col-12">
+          <div className="anime-info">
+            <span className="font-semibold">Studio: </span>
+
+            {data.data.data.studios.map((item) => item.name).join(" ")}
+          </div>
+        </div>
+        <div className="xl:col-6 md:col-6 sm:col-12">
+          <div className="anime-info">
+            <span className="font-semibold">Tags: </span>
+            {data.data.data.genres.map((item) => item.name).join(" ")}
+          </div>
+        </div>
+      </div>
+      <div className="xl:col-12 anime-desc">
+        <div className="anime-info">{data.data.data.synopsis}</div>
+      </div>
+    </div>
+  );
+};
+
 export default Info;
